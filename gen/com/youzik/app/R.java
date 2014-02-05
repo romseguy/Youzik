2e480b535fa4cddb47c2763af381dc9217caabda
worm <rom.seguy@gmail.com>
2014-02-05 14:54:35 +0100

    Download fragment part1

--- gen/com/youzik/app/R.java
+++ gen/com/youzik/app/R.java
@@ -30,7 +30,8 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_main_dummy=0x7f030001;
+        public static final int download_item=0x7f030001;
+        public static final int fragment_main_dummy=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/com/youzik/app/fragments/DownloadTabFragment.java
+++ src/com/youzik/app/fragments/DownloadTabFragment.java
@@ -1,7 +1,65 @@
 package com.youzik.app.fragments;
 
-import android.support.v4.app.Fragment;
+import java.util.ArrayList;
+import java.util.List;
 
-public class DownloadTabFragment extends Fragment {
+import com.youzik.app.R;
+import com.youzik.app.entities.Download;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+public class DownloadTabFragment extends ListFragment {
+
+	private DownloadsAdapter adapter;
+	private List<Download> data;
+	
+	public class DownloadsAdapter extends ArrayAdapter<Download> {
+
+		public DownloadsAdapter(Context context, List<Download> data) {
+			super(context, R.layout.download_item, data);
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			if (convertView == null) {
+				LayoutInflater inflater = ((Activity) this.getContext()).getLayoutInflater();
+                convertView = inflater.inflate(R.layout.download_item, parent, false);
+			}
+			
+			// display item content on download_list_element layout
+			final Download item = this.getItem(position);
+			return convertView;
+		}
+		
+	}
+
+	private void createList() {
+		this.data = new ArrayList<Download>();
+		this.adapter = new DownloadsAdapter(this.getActivity(), this.data);
+		this.setListAdapter(this.adapter);
+		this.updateList();
+	}
+	
+	private void updateList() {
+		if (this.data == null || this.adapter == null)
+			this.createList();
+		
+		this.data.clear();
+		/** @TODO this.data.addAll(getDownloads()) */
+		this.adapter.notifyDataSetChanged();
+	}
+	
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+    	super.onActivityCreated(savedInstanceState);
+    	this.createList();
+    }
 
 }
