ee7ae74c7c9fc7ed5e99cbbd65f54ebbb9ffa7ef
worm <rom.seguy@gmail.com>
2014-02-14 04:41:37 +0100

    Music player

--- gen/com/youzik/app/R.java
+++ gen/com/youzik/app/R.java
@@ -21,23 +21,82 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int adele=0x7f020000;
+        public static final int bg=0x7f020001;
+        public static final int btn_backward=0x7f020002;
+        public static final int btn_forward=0x7f020003;
+        public static final int btn_next=0x7f020004;
+        public static final int btn_pause=0x7f020005;
+        public static final int btn_play=0x7f020006;
+        public static final int btn_playlist=0x7f020007;
+        public static final int btn_previous=0x7f020008;
+        public static final int btn_repeat=0x7f020009;
+        public static final int btn_repeat_focused=0x7f02000a;
+        public static final int btn_shuffle=0x7f02000b;
+        public static final int btn_shuffle_focused=0x7f02000c;
+        public static final int gradient_bg=0x7f02000d;
+        public static final int gradient_bg_hover=0x7f02000e;
+        public static final int ic_launcher=0x7f02000f;
+        public static final int img_btn_backward=0x7f020010;
+        public static final int img_btn_backward_pressed=0x7f020011;
+        public static final int img_btn_forward=0x7f020012;
+        public static final int img_btn_forward_pressed=0x7f020013;
+        public static final int img_btn_next=0x7f020014;
+        public static final int img_btn_next_pressed=0x7f020015;
+        public static final int img_btn_pause=0x7f020016;
+        public static final int img_btn_pause_pressed=0x7f020017;
+        public static final int img_btn_play=0x7f020018;
+        public static final int img_btn_play_pressed=0x7f020019;
+        public static final int img_btn_playlist=0x7f02001a;
+        public static final int img_btn_playlist_pressed=0x7f02001b;
+        public static final int img_btn_previous=0x7f02001c;
+        public static final int img_btn_previous_pressed=0x7f02001d;
+        public static final int img_btn_repeat=0x7f02001e;
+        public static final int img_btn_repeat_pressed=0x7f02001f;
+        public static final int img_btn_shuffle=0x7f020020;
+        public static final int img_btn_shuffle_pressed=0x7f020021;
+        public static final int img_seekbar_bg=0x7f020022;
+        public static final int img_seekbar_progress_blue=0x7f020023;
+        public static final int list_selector=0x7f020024;
+        public static final int seek_handler=0x7f020025;
+        public static final int seekbar_progress=0x7f020026;
+        public static final int seekbar_progress_bg=0x7f020027;
+        public static final int track_info_bg=0x7f020028;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
+        public static final int action_settings=0x7f080015;
         public static final int browse_tab=0x7f080001;
+        public static final int btnBackward=0x7f08000b;
+        public static final int btnForward=0x7f08000d;
+        public static final int btnNext=0x7f08000e;
+        public static final int btnPlay=0x7f08000c;
+        public static final int btnPrevious=0x7f08000a;
+        public static final int btnRepeat=0x7f080013;
+        public static final int btnShuffle=0x7f080014;
         public static final int download_item_name=0x7f080003;
         public static final int pager=0x7f080000;
+        public static final int player_footer_bg=0x7f080009;
+        public static final int player_header_bg=0x7f080006;
         public static final int progressbar=0x7f080004;
         public static final int progressbars=0x7f080005;
+        public static final int songCurrentDurationLabel=0x7f080011;
+        public static final int songProgressBar=0x7f08000f;
+        public static final int songThumbnail=0x7f080008;
+        public static final int songTitle=0x7f080007;
+        public static final int songTotalDurationLabel=0x7f080012;
+        public static final int timerDisplay=0x7f080010;
         public static final int webView=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int browse_tab=0x7f030001;
-        public static final int download_item=0x7f030002;
-        public static final int download_progressbar=0x7f030003;
-        public static final int download_tab=0x7f030004;
+        public static final int bg_player_footer=0x7f030001;
+        public static final int bg_player_header=0x7f030002;
+        public static final int browse_tab=0x7f030003;
+        public static final int download_item=0x7f030004;
+        public static final int download_progressbar=0x7f030005;
+        public static final int download_tab=0x7f030006;
+        public static final int play_tab=0x7f030007;
+        public static final int rounded_corner=0x7f030008;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/com/youzik/app/fragments/PlayTabFragment.java
+++ src/com/youzik/app/fragments/PlayTabFragment.java
@@ -1,7 +1,308 @@
 package com.youzik.app.fragments;
 
-import android.support.v4.app.Fragment;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
 
-public class PlayTabFragment extends Fragment {
+import com.youzik.app.R;
+import com.youzik.app.helpers.Convert;
+
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class PlayTabFragment extends Fragment implements
+		OnCompletionListener, SeekBar.OnSeekBarChangeListener {
+	
+	private ImageButton btnPlay;
+	private ImageButton btnForward;
+	private ImageButton btnBackward;
+	private ImageButton btnNext;
+	private ImageButton btnPrevious;
+	//private ImageButton btnPlaylist;
+	private ImageButton btnRepeat;
+	private ImageButton btnShuffle;
+	private SeekBar songProgressBar;
+	private TextView songTitleLabel;
+	private TextView songCurrentDurationLabel;
+	private TextView songTotalDurationLabel;
+	
+	private MediaPlayer mp;
+	private Handler mHandler = new Handler();
+	private ArrayList<HashMap<String, String>> songsList = new ArrayList<HashMap<String, String>>();
+	//private SongsManager songManager;
+	
+	private int seekForwardTime = 5000; // ms
+	private int seekBackwardTime = 5000; // ms
+	private int currentSongIndex = 0; 
+	private boolean isShuffle = false;
+	private boolean isRepeat = false;
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		this.mp = new MediaPlayer();
+		this.mp.setOnCompletionListener(this);
+		this.songProgressBar.setOnSeekBarChangeListener(this);
+		
+		btnPlay.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				if (mp == null) return;
+				
+				if (mp.isPlaying()) {
+					mp.pause();
+					btnPlay.setImageResource(R.drawable.btn_play);
+				} else {
+					btnPlay.setImageResource(R.drawable.btn_pause);
+				}
+			}
+		});
+		
+		btnForward.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				int currentPosition = mp.getCurrentPosition();
+				
+				// check if seekForward time is lesser than song duration
+				if (currentPosition + seekForwardTime <= mp.getDuration()) {
+					// forward song
+					mp.seekTo(currentPosition + seekForwardTime);
+				} else {
+					// forward to end position
+					mp.seekTo(mp.getDuration());
+				}
+			}
+		});
+		
+		btnBackward.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				int currentPosition = mp.getCurrentPosition();
+				// check if seekBackward time is greater than 0 sec
+				if (currentPosition - seekBackwardTime >= 0) {
+					// forward song
+					mp.seekTo(currentPosition - seekBackwardTime);
+				} else {
+					// backward to starting position
+					mp.seekTo(0);
+				}
+			}
+		});
+		
+		btnNext.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				// check if next song is there or not
+				if (currentSongIndex < (songsList.size() - 1)) {
+					playSong(currentSongIndex + 1);
+					currentSongIndex = currentSongIndex + 1;
+				} else {
+					// play first song
+					playSong(0);
+					currentSongIndex = 0;
+				}
+			}
+		});
+		
+		btnPrevious.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				if (currentSongIndex > 0) {
+					playSong(currentSongIndex - 1);
+					currentSongIndex = currentSongIndex - 1;
+				} else {
+					// play last song
+					playSong(songsList.size() - 1);
+					currentSongIndex = songsList.size() - 1;
+				}
+			}
+		});
+		
+		btnRepeat.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				if (isRepeat) {
+					isRepeat = false;
+					Toast.makeText(getActivity().getApplicationContext(), "Repeat is OFF", Toast.LENGTH_SHORT).show();
+					btnRepeat.setImageResource(R.drawable.btn_repeat);
+				} else {
+					isRepeat = true;
+					Toast.makeText(getActivity().getApplicationContext(), "Repeat is ON", Toast.LENGTH_SHORT).show();
+					isShuffle = false;
+					btnRepeat.setImageResource(R.drawable.btn_repeat_focused);
+					btnShuffle.setImageResource(R.drawable.btn_shuffle);
+				}
+			}
+		});
+		
+		btnShuffle.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				if (isShuffle) {
+					isShuffle = false;
+					Toast.makeText(getActivity().getApplicationContext(), "Shuffle is OFF", Toast.LENGTH_SHORT).show();
+					btnShuffle.setImageResource(R.drawable.btn_shuffle);
+				} else {
+					isShuffle = true;
+					Toast.makeText(getActivity().getApplicationContext(), "Shuffle is ON", Toast.LENGTH_SHORT).show();
+					isRepeat = false;
+					btnShuffle.setImageResource(R.drawable.btn_shuffle_focused);
+					btnRepeat.setImageResource(R.drawable.btn_repeat);
+				}
+			}
+		});
+		
+		/**
+		 * Button Click event for Play list click event Launches list activity
+		 * which displays list of songs
+		 */
+		/*btnPlaylist.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				Intent i = new Intent(getActivity().getApplicationContext(), PlayListActivity.class);
+				startActivityForResult(i, 100);
+			}
+		});*/
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View playTabView = inflater.inflate(R.layout.play_tab, container, false);
+		this.btnPlay = (ImageButton) playTabView.findViewById(R.id.btnPlay);
+		this.btnForward = (ImageButton) playTabView.findViewById(R.id.btnForward);
+		this.btnBackward = (ImageButton) playTabView.findViewById(R.id.btnBackward);
+		this.btnNext = (ImageButton) playTabView.findViewById(R.id.btnNext);
+		this.btnPrevious = (ImageButton) playTabView.findViewById(R.id.btnPrevious);
+		//this.btnPlaylist = (ImageButton) playTabView.findViewById(R.id.btnPlaylist);
+		this.btnRepeat = (ImageButton) playTabView.findViewById(R.id.btnRepeat);
+		this.btnShuffle = (ImageButton) playTabView.findViewById(R.id.btnShuffle);
+		this.songProgressBar = (SeekBar) playTabView.findViewById(R.id.songProgressBar);
+		this.songTitleLabel = (TextView) playTabView.findViewById(R.id.songTitle);
+		this.songCurrentDurationLabel = (TextView) playTabView.findViewById(R.id.songCurrentDurationLabel);
+		this.songTotalDurationLabel = (TextView) playTabView.findViewById(R.id.songTotalDurationLabel);
+		return playTabView;
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		mp.release();
+	}
+	
+	private Runnable mUpdateTimeTask = new Runnable() {
+	   public void run() {
+		   long totalDuration = mp.getDuration();
+		   long currentDuration = mp.getCurrentPosition();
+		  
+		   // Displaying Total Duration time
+		   songTotalDurationLabel.setText(""+Convert.milliSecondsToTimer(totalDuration));
+		   // Displaying time completed playing
+		   songCurrentDurationLabel.setText(""+Convert.milliSecondsToTimer(currentDuration));
+		   
+		   // Updating progress bar
+		   int progress = (int)(Convert.getProgressPercentage(currentDuration, totalDuration));
+		   //Log.d("Progress", ""+progress);
+		   songProgressBar.setProgress(progress);
+		   
+		   // Running this thread after 100 milliseconds
+	       mHandler.postDelayed(this, 100);
+	   }
+	};
+	
+	/**
+	 * Receiving song index from playlist view and play the song
+	 * */
+	/*@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		if (resultCode == 100) {
+			currentSongIndex = data.getExtras().getInt("songIndex");
+			playSong(currentSongIndex);
+		}
+	}*/
+	
+	public void playSong(int songIndex) {
+		try {
+			mp.reset();
+			mp.setDataSource(songsList.get(songIndex).get("songPath"));
+			mp.prepare();
+			mp.start();
+			
+			String songTitle = songsList.get(songIndex).get("songTitle");
+			songTitleLabel.setText(songTitle);
+			btnPlay.setImageResource(R.drawable.btn_pause);
+			
+			songProgressBar.setProgress(0);
+			songProgressBar.setMax(100);
+			updateProgressBar();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void updateProgressBar() {
+        mHandler.postDelayed(mUpdateTimeTask, 100);        
+    }
+	
+	@Override
+	public void onCompletion(MediaPlayer arg0) {
+		// check for repeat is ON or OFF
+		if (isRepeat) {
+			// repeat is on play same song again
+			playSong(currentSongIndex);
+		} else if (isShuffle) {
+			// shuffle is on - play a random song
+			Random rand = new Random();
+			currentSongIndex = rand.nextInt((songsList.size() - 1) - 0 + 1) + 0;
+			playSong(currentSongIndex);
+		} else {
+			// no repeat or shuffle ON - play next song
+			if (currentSongIndex < (songsList.size() - 1)) {
+				playSong(currentSongIndex + 1);
+				currentSongIndex = currentSongIndex + 1;
+			} else {
+				// play first song
+				playSong(0);
+				currentSongIndex = 0;
+			}
+		}
+	}
+
+	/**
+	 * When user starts moving the progress handler
+	 */
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) {
+		mHandler.removeCallbacks(mUpdateTimeTask);
+	}
+
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) {
+		mHandler.removeCallbacks(mUpdateTimeTask);
+		int totalDuration = mp.getDuration();
+		int currentPosition = Convert.progressToTimer(seekBar.getProgress(), totalDuration);
+		
+		// forward or backward to certain seconds
+		mp.seekTo(currentPosition);
+		
+		// update timer progress again
+		updateProgressBar();
+	}
+	
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {		
+	}
 
 }
